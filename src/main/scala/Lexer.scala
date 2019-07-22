0568eaad5eb377eeda2db061dad94665b5c4821a
Luke-Dramko <luke.dramko@gmail.com>
2019-07-22 17:34:18 -0400

    Updated access specifiers.

--- src/main/scala/Lexer.scala
+++ src/main/scala/Lexer.scala
@@ -10,131 +10,131 @@ object Lexer extends RegexParsers {
     """[-a-zA-Z!%&|!<>*_=+][-a-zA-Z0-9!%&|!<>*_=+]*""".r ^^ { str => Identifier(str) }
   }
 
-  def intconstant: Parser[IntConstant] = {
+  private[this] def intconstant: Parser[IntConstant] = {
     "[0-9]+".r ^^ { str => IntConstant(str) }
   }
 
-  def floatconstant: Parser[FloatConstant] = {
+  private[this] def floatconstant: Parser[FloatConstant] = {
     """[0-9]+\.([0-9]*)?""".r ^^ { str => FloatConstant(str) }
   }
 
-  def stringconstant: Parser[StringConstant] = {
+  private[this] def stringconstant: Parser[StringConstant] = {
     """"([^"]|\")*"""".r ^^ { str => StringConstant(str.substring(1, str.length - 1)) }
   }
 
-  def boolconstant: Parser[BoolConstant] = {
+  private[this] def boolconstant: Parser[BoolConstant] = {
     "(true)|(false)".r ^^ { str => BoolConstant(str) }
   }
-  
-  def _if: Parser[Token] = {
+
+  private[this] def _if: Parser[Token] = {
 	  "if" ^^ { str => If}
   }
-  
-  def _else: Parser[Token] = {
+
+  private[this] def _else: Parser[Token] = {
 	  "else" ^^ { str => Else }
   }
-  
-  def _let: Parser[Token] = {
+
+  private[this] def _let: Parser[Token] = {
 	  "let" ^^ { str => Let }
   }
-  
-  def _case: Parser[Token] = {
+
+  private[this] def _case: Parser[Token] = {
 	  "case" ^^ { str => Case }
   }
-  
-  def _default: Parser[Token] = {
+
+  private[this] def _default: Parser[Token] = {
 	  "default" ^^ { str => Default }
   }
-  
-  def _import: Parser[Token] = {
+
+  private[this] def _import: Parser[Token] = {
 	  "import" ^^ { str => Import }
   }
-  
-  def _typedef: Parser[Token] = {
+
+  private[this] def _typedef: Parser[Token] = {
 	  "typedef" ^^ { str => Typedef }
   }
-  
-  def _as: Parser[Token] = {
+
+  private[this] def _as: Parser[Token] = {
 	  "as" ^^ { str => As }
   }
-  
-  def _def: Parser[Token] = {
+
+  private[this] def _def: Parser[Token] = {
 	  "def" ^^ { str => Def }
   }
-  
-  def _int: Parser[Token] = {
+
+  private[this] def _int: Parser[Token] = {
 	  "Int" ^^ { str => _Int }
   }
-  
-  def _uint: Parser[Token] = {
+
+  private[this] def _uint: Parser[Token] = {
 	  "UInt" ^^ { str => _UInt }
   }
-  
-  def _float: Parser[Token] = {
+
+  private[this] def _float: Parser[Token] = {
 	  "Float" ^^ { str => _Float }
   }
-  
-  def _string: Parser[Token] = {
+
+  private[this] def _string: Parser[Token] = {
 	  "String" ^^ { str => _String }
   }
-  
-  def _bool: Parser[Token] = {
+
+  private[this] def _bool: Parser[Token] = {
 	  "Bool" ^^ { str => _Bool }
   }
-  
-  def _enum: Parser[Token] = {
+
+  private[this] def _enum: Parser[Token] = {
 	  "Enum" ^^ { str => _Enum }
   }
-  
-  def enumValue: Parser[Token] = {
+
+  private[this] def enumValue: Parser[Token] = {
 	  "enum" ^^ { str => EnumValue }
   }
-  
-  def _null: Parser[Token] = {
+
+  private[this] def _null: Parser[Token] = {
 	  "Null" ^^ { str => _Null }
   }
-  
-  def nullValue: Parser[Token] = {
+
+  private[this] def nullValue: Parser[Token] = {
 	  "null" ^^ { str => NullValue }
   }
-  
-  def arrow: Parser[Token] = {
+
+  private[this] def arrow: Parser[Token] = {
 	  "->" ^^ { str => Arrow }
   }
-  
-  def equalsSign: Parser[Token] = {
+
+  private[this] def equalsSign: Parser[Token] = {
 	  "=" ^^ { str => EqualsSign }
   }
-  
-  def newLine: Parser[Token] = {
+
+  private[this] def newLine: Parser[Token] = {
 	  "\n" ^^ { str => NewLine }
   }
 
-  def lParen: Parser[Token] = {
+  private[this] def lParen: Parser[Token] = {
     "(" ^^ { str => LParen }
   }
 
-  def rParen: Parser[Token] = {
+  private[this] def rParen: Parser[Token] = {
     ")" ^^ { str => RParen }
   }
 
-  def lCurlyBrace: Parser[Token] = {
+  private[this] def lCurlyBrace: Parser[Token] = {
     "{" ^^ { str => LCurlyBrace }
   }
 
-  def rCurlyBrace: Parser[Token] = {
+  private[this] def rCurlyBrace: Parser[Token] = {
     "}" ^^ { str => RCurlyBrace }
   }
 
-  def lBracket: Parser[Token] = {
+  private[this] def lBracket: Parser[Token] = {
     "[" ^^ { str => LBracket }
   }
 
-  def rBracket: Parser[Token] = {
+  private[this] def rBracket: Parser[Token] = {
     "]" ^^ { str => RBracket }
   }
 
-  def comma: Parser[Token] = {
+  private[this] def comma: Parser[Token] = {
     "," ^^ { str => Comma }
   }
 
